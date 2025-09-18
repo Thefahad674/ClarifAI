d7a880cf7e1ef5ff5dd90d9adc67ee9481ea2a6b
Thefahad674 <fahadf30511@gmail.com>
2025-09-18 12:51:43 +0530

    deploying client

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -4,21 +4,19 @@ import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen overflow-hidden flex flex-col bg-gradient-to-br from-black via-neutral-00 to-black">
+    <div className="h-[89vh] w-screen overflow-hidden flex flex-col bg-gradient-to-br from-black via- -900 to-black">
       {/* Navbar */}
-      <div className="h-[60px] flex-shrink-0">
-        <Navbar />
-      </div>
+       
 
       {/* Main content */}
-      <div className="flex flex-1 overflow-hidden">
+      <div className="flex flex-1 overflow-hidden flex-col lg:flex-row">
         {/* Left Panel */}
-        <div className="w-[30vw] h-full p-4 flex justify-center items-center">
+        <div className="w-full lg:w-[30vw] h-[40vh] lg:h-full p-4 flex justify-center items-center">
           <FileUploadComponent />
         </div>
 
         {/* Right Panel */}
-        <div className="w-[70vw] h-full border-none  overflow-hidden rounded-2xl">
+        <div className="w-full lg:w-[70vw] h-[60vh] lg:h-full border-none overflow-hidden rounded-2xl">
           <ChatComponent />
         </div>
       </div>
